1f99be53a587ad510a015fa5355e14c003349f4f
good665 <good66591@gmail.com>
2021-07-04 17:22:48 +0900

    3번째 flutter

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xFF535353);
+const kTextLightColor = Color(0xFFACACAC);
+
+const kDefaultPaddin = 20.0;
+
+const kTextColor2 = Color(0xFF171717);
+const kPrimaryColor = Color(0xFFA95EFA);
+const kSecondaryColor = Color(0xFFF3F6F8);
---
+++ lib/screens/home/home_screen2.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
